081b8696af02c7512ae55d8813f104c36509ffdd
julio1927 <jrivas6219@conestogac.on.ca>
2020-05-07 02:27:26 -0400

    implemented authenticated user can edit profile

--- app/Profile.php
+++ app/Profile.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+    protected $guarded = [];
+
     public function user()
     {
         return $this->belongsTo(User::class); // Makes 1:1 relationship with user and profile 
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -14,8 +14,15 @@
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{ $user->username}}</h1>
-                <a href="/p/create">Add New Post</a>
+                <a href="/p/create">Add New Post</a> 
             </div>
+
+            <!-- Edit Profile --> 
+            <div>
+                <a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
+            </div>
+
+            <!-- User Profile Stats -->
             <div class="d-flex">
                 <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
                 <div class="pr-5"><strong>400</strong> follwers</div>
