8ee370c180d7136a5e7971c0fe20b6524991d250
julio1927 <jrivas6219@conestogac.on.ca>
2020-05-08 15:24:00 -0400

    Updated UI for displaying posts with bootstrap

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 <div class="container">
+    <!-- The purpose of this view is to display the post -->
     <div class="row">
         <!-- Image -->
         <div class="col-8">
@@ -10,8 +11,35 @@
 
         <!-- Username and Caption -->
         <div class="col-4">
-            <h3>{{ $post->user->username }}</h3>
-            <p> {{ $post-caption }}</p>
+            <div>
+                <div class="d-flex align-items-center">
+                    <!-- User profile image -->
+                    <div class="pr-3">
+                        <img src="/storage/{{ $post->user->profile->image }}" class="rounded-circle w-100" style="max-width: 40px;">
+                    </div>
+
+                    <!-- User Username -->
+                    <div>
+                        <div class="font-weight-bold">
+                            <a href="/profile/{{ $post->user->id }}">
+                                <span class="text-dark">{{ $post->user->username }}</span>
+                            </a>
+                            <a href="#" class="pl-3">Follow</a>
+                        </div>
+                    </div>
+                </div>
+
+                <hr>
+                
+                <!-- Username + caption -->
+                <p>
+                    <span class="font-weight-bold">
+                        <a href="/profile/{{ $post->user->id }}">
+                            <span class="text-dark">{{ $post->user->username }}</span>
+                        </a>
+                    </span> {{ $post->caption }}
+                </p>
+            </div>
         </div>
     </div>
 </div>
